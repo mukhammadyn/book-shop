a1838096bf9258d4d5f4b9e9837ca7837cee3804
Mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2021-09-22 18:58:33 +0500

    Add search modal

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 const elNavToggler = document.querySelector('.js-nav-toggler');
 const elNavigation = document.querySelector('.header-navigation__nav');
+const elSearchButton = document.querySelector('.js-search-button');
+const elSearchModal = document.querySelector('.search-modal');
 
 function toggleNavActive() {
   elNavigation.classList.toggle('header-navigation__nav--active');
 }
 
+function openSearchModal() {
+  elSearchModal.classList.add('search-modal--open');
+}
+
 if (elNavToggler) {
   elNavToggler.addEventListener('click', toggleNavActive);
+}
+
+if (elSearchButton) {
+  elSearchButton.addEventListener('click', openSearchModal);
+}
+
+window.onclick = function(evt) {
+  if (evt.target == elSearchModal) {
+    elSearchModal.classList.remove('search-modal--open');
+  }
 }
